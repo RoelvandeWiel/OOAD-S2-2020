93884f0e25eed3865a731e6efae9183809509b90
jesse-28 <jesse-28@hotmail.com>
2020-05-28 21:47:07 +0200

    vragenlijsten

--- vagado/src/main/java/ooad/DAO/VragenLijstDAO.java
+++ vagado/src/main/java/ooad/DAO/VragenLijstDAO.java
@@ -2,17 +2,20 @@ package ooad.DAO;
 
 import ooad.DTO.GebruikerDTO;
 import ooad.DTO.VragenlijstDTO;
+import ooad.Database.Database;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class VragenLijstDAO {
+    private Database database = new Database();
 
     public List<VragenlijstDTO> getVragenlijsten(GebruikerDTO gebruiker){
-
+        return new ArrayList<VragenlijstDTO>();
     }
 
     public void koopVragenLijst(VragenlijstDTO vragenlijst, GebruikerDTO gebruiker){
-
+        gebruiker.vragenlijsten.add(vragenlijst);
     }
 
 }
